685f7ee4768b4d15ebbaaf186350d77e510ffb9b
XXL Steve <80220423+CornetPanique86@users.noreply.github.com>
2024-03-12 21:17:37 +0100

    ok

--- ug/bedwars.d.ts
+++ ug/bedwars.d.ts
@@ -1,3 +1,5 @@
 import { CommandOutput } from "bdsx/bds/command";
 import { CommandOrigin } from "bdsx/bds/commandorigin";
-export declare function bedwarsstart(param: {}, origin: CommandOrigin, output: CommandOutput): Promise<void>;
+export declare function bedwarsstart(param: {
+    option: string;
+}, origin: CommandOrigin, output: CommandOutput): Promise<void>;
--- ug/utils.d.ts
+++ ug/utils.d.ts
@@ -2,6 +2,25 @@ import { Player } from "bdsx/bds/player";
 import { Games } from ".";
 import { CommandOrigin } from "bdsx/bds/commandorigin";
 import { CommandOutput } from "bdsx/bds/command";
+import { ItemStack } from "bdsx/bds/inventory";
+import { EnchantmentNames } from "bdsx/bds/enchants";
+export declare function getPlayerByName(name: string): Player | null;
+type ItemDesc = {
+    item: string;
+    amount?: number;
+    data?: number;
+    name?: string;
+    lore?: string[];
+    enchantment?: {
+        enchant: EnchantmentNames;
+        level: number;
+        isUnsafe: boolean;
+    };
+};
+export declare function createCItemStack(item: ItemDesc): ItemStack;
+export declare function stopGame(): void;
 export declare function startGame(game: Games, players: Player[], sec: number, title?: string): Promise<string[] | null>;
+export declare function countdownActionbar(sec: number, pls: string[], actionbar: boolean, title?: string): Promise<boolean>;
 export declare function joinqueue(origin: CommandOrigin, output: CommandOutput): void;
 export declare function leavequeue(origin: CommandOrigin, output: CommandOutput): void;
+export {};
